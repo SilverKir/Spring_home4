e860f14b95d1ee4ae71279dcb43fc8196d4c6845
silver_kir <rci@mp03.ru>
2024-11-25 10:10:09 +0800

    initial commit

---
+++ src/main/java/com/example/sem4home/Sem4homeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sem4home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4homeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem4homeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sem4home/Sem4homeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem4home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4homeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
